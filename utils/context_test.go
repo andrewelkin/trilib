8abe08897f20a1d01c9aba1031864e025cf8d944
Ilya Biin <ilya@biin.dev>
2023-10-19 20:44:11 +0800

    context is tested

--- utils/context_test.go
+++ utils/context_test.go
@@ -14,35 +14,57 @@ func TestGetOrCreateGlobalContext(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
+	acceptString := "accept"   // The logger will only accept this namespace
+	excludeString := "exclude" // The logger will exclude this namespace
+	underscoreString := "_testNS"
+
 	tests := []struct {
-		Actor  func(*ContextWithCancel)
-		Ns     string
-		Result string
+		Actor    func(*ContextWithCancel)
+		Result   string
+		GFilter  *string
+		GExclude *string
 	}{
 		{func(ctx *ContextWithCancel) {
-			ctx.Logger.Debugf("_PrivateNS", "Global logger created")
-		}, "_PrivateNS", ""},
+			ctx.Logger.Debugf(acceptString, "test")
+			ctx.Logger.Debugf(excludeString, "test")
+			ctx.Logger.Debugf(underscoreString, "test")
+		}, "accept test\nexclude test\n", nil, nil},
 		{func(ctx *ContextWithCancel) {
-			ctx.Logger.Debugf("*", "Global logger created")
-		}, "*", ""},
-	}
+			ctx.Logger.Debugf(acceptString, "test")
+			ctx.Logger.Debugf(excludeString, "test")
+			ctx.Logger.Debugf(underscoreString, "test")
+		}, "accept test\n", &acceptString, nil},
+		{func(ctx *ContextWithCancel) {
+			ctx.Logger.Debugf(acceptString, "test")
+			ctx.Logger.Debugf(excludeString, "test")
+			ctx.Logger.Debugf(underscoreString, "test")
 
+		}, "", &acceptString, &acceptString},
+	}
 	for _, test := range tests {
 		buf := new(strings.Builder)
 		logger.SetDefaultScreenIO(buf)
 		globalContext = nil
+		var gfilter logger.FilterFunc
 
 		globalLogger, natsLogger := logger.NewMockLogger(ctrl), logger.NewMockLogger(ctrl)
-		globalLogger.EXPECT().AddOutput(gomock.Any(), gomock.Any(), logger.LogLevel(0), false, false)
+		globalLogger.EXPECT().AddOutput(gomock.Any(), gomock.Any(), logger.LogLevel(0), false, false).Do(
+			func(filter logger.FilterFunc, output interface{}, minLevel logger.LogLevel, ansi bool, trailCR bool, opts ...interface{}) {
+				gfilter = filter
+			})
 		globalLogger.EXPECT().Infof("*", "Creating global context with default logger level %v and namespace %v", gomock.Any(), gomock.Any())
-		globalLogger.EXPECT().Debugf(test.Ns, "Global logger created")
-		ctx := CreateMockedContext(ctrl, nil, nil, nil, nil, []*logger.MockLogger{globalLogger, globalLogger, natsLogger})
+		globalLogger.EXPECT().Debugf(gomock.Any(), gomock.Any()).Do(func(namespace, format string, a ...interface{}) {
+			if gfilter(namespace) {
+				buf.WriteString(namespace + " " + format + "\n")
+			}
+		}).AnyTimes()
+		ctx := CreateMockedContext(ctrl, test.GFilter, test.GExclude, nil, nil, []*logger.MockLogger{globalLogger, globalLogger, natsLogger})
 		test.Actor(ctx)
 
 		time.Sleep(time.Millisecond * 100)
 
 		if buf.String() != test.Result {
-			t.Errorf("Unexpected output: \n%s\n vs \n%s\n", buf.String(), test.Result)
+			t.Errorf("Unexpected output: `%s` vs `%s`", buf.String(), test.Result)
 		}
 	}
 }
@@ -56,11 +78,11 @@ func CreateMockedContext(ctrl *gomock.Controller, natsFilter, natsExclude, gFilt
 	mockedNatsConfig := NewMockIConfig(ctrl)
 	mockedNatsConfig.EXPECT().GetStringDefault("subject", "default-logger-subject").Return(&frog)
 	mockedNatsConfig.EXPECT().GetStringDefault("url", nats.DefaultURL).Return(&url)
-	mockedNatsConfig.EXPECT().GetString("filter").Return(natsFilter)
-	mockedNatsConfig.EXPECT().GetString("exclude").Return(natsExclude)
-	mockedNatsConfig.EXPECT().GetStringDefault("logLevel", "debug").Return(&debug)
-	mockedNatsConfig.EXPECT().GetBoolDefault("ansicodes", false).Return(false)
-	mockedNatsConfig.EXPECT().GetStringDefault("natsSeed", "").Return(&empty)
+	mockedNatsConfig.EXPECT().GetString("filter").Return(natsFilter).AnyTimes()
+	mockedNatsConfig.EXPECT().GetString("exclude").Return(natsExclude).AnyTimes()
+	mockedNatsConfig.EXPECT().GetStringDefault("logLevel", "debug").Return(&debug).AnyTimes()
+	mockedNatsConfig.EXPECT().GetBoolDefault("ansicodes", false).Return(false).AnyTimes()
+	mockedNatsConfig.EXPECT().GetStringDefault("natsSeed", "").Return(&empty).AnyTimes()
 
 	mockedOutputsConfig := NewMockIConfig(ctrl)
 	mockedOutputsConfig.EXPECT().GetCfg().Return(map[string]interface{}{
